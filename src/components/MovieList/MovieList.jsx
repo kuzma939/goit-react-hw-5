0971f3ecfbf024ea20eab662e8fd6c15a3496411
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-06-07 09:39:46 +0200

    react

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,19 @@
-import { NavLink } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './MovieList.module.css';
 
-const MovieList = ({ movies }) => (
-  <ul className={styles.list}>
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+  return (
+    <ul className={styles.list}>
     {movies.map(movie => (
       <li key={movie.id} className={styles.item}>
-        <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+        <Link to={`/movies/${movie.id}`} state={{ from: location }}>{movie.title}</Link>
       </li>
     ))}
   </ul>
-);
+  )
+ 
+};
 
 export default MovieList;
 
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import MovieList from '../../components/MovieList/MovieList';
 import styles from './MoviesPage.module.css';
 import { searchMovies } from '../../api';
 
 const MoviesPage = () => {
-    const [query, setQuery] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+    const [query, setQuery] = useState(searchParams.get('query') || '');
     const [movies, setMovies] = useState([]);
+    useEffect(() => {
+      if (query) {
+        fetchMovies(query);
+      }
+    }, [query]);
   
+    const fetchMovies = async (query) => {
+      try {
+        searchMovies(query).then(setMovies);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
     const handleSearch = async (e) => {
       e.preventDefault();
-      searchMovies(query).then(setMovies);
+      const form = e.target;
+      const newQuery = form.elements.query.value;
+      setSearchParams({ query: newQuery });
+      setQuery(newQuery);
     
     };
   
     return (
       <div className={styles.container}>
-        <h1>Search Movies</h1>
-        <form onSubmit={handleSearch} className={styles.form}>
-          <input
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            className={styles.input}
-            placeholder="Enter movie name"
-          />
-          <button type="submit" className={styles.button}>Search</button>
-        </form>
-        <MovieList movies={movies} />
-      </div>
+      <form onSubmit={handleSearch} className={styles.form}>
+        <input type="text" name="query" defaultValue={query} />
+        <button type="submit" className={styles.button}>Search</button>
+      </form>
+      <MovieList movies={movies} />
+    </div>
     );
   };
   
   export default MoviesPage;
+ 
